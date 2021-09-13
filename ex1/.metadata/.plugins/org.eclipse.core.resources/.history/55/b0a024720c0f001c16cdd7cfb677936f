340871bca826150f50ac5a73117192a7d9cc5ab6
omer <omergonen13@gmail.com>
2021-09-13 09:16:05 +0300

    first commit

---
+++ ex1/.metadata/.plugins/org.eclipse.core.resources/.history/55/b0a024720c0f001c16cdd7cfb677936f
@@ -0,0 +1,2 @@
+module testNG {
+}
---
+++ ex1/ex1/src/ex1/t1.java
@@ -0,0 +1,13 @@
+package ex1;
+
+
+import org.testng.annotations.Test;
+
+public class t1 {
+	
+	@Test
+	public void Demo()
+	{
+		System.out.println("hello test");
+	}
+}
